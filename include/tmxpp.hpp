9d12c03263a940e22d33ca09b23fb7f0e55b7643
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-01-28 20:10:41 -0400

    Add tmxpp.hpp, a header with the whole public API

---
+++ include/tmxpp.hpp
@@ -0,0 +1,31 @@
+#ifndef TMXPP_HPP
+#define TMXPP_HPP
+
+#include <tmxpp/Animation.hpp>
+#include <tmxpp/Color.hpp>
+#include <tmxpp/Data.hpp>
+#include <tmxpp/Degrees.hpp>
+#include <tmxpp/File.hpp>
+#include <tmxpp/Flip.hpp>
+#include <tmxpp/Flipped_gid.hpp>
+#include <tmxpp/Frame.hpp>
+#include <tmxpp/Image.hpp>
+#include <tmxpp/Image_collection.hpp>
+#include <tmxpp/Image_layer.hpp>
+#include <tmxpp/Layer.hpp>
+#include <tmxpp/Map.hpp>
+#include <tmxpp/Object.hpp>
+#include <tmxpp/Object_layer.hpp>
+#include <tmxpp/Offset.hpp>
+#include <tmxpp/Pixel.hpp>
+#include <tmxpp/Point.hpp>
+#include <tmxpp/Properties.hpp>
+#include <tmxpp/Property.hpp>
+#include <tmxpp/Size.hpp>
+#include <tmxpp/Tile_id.hpp>
+#include <tmxpp/Tile_layer.hpp>
+#include <tmxpp/Tile_set.hpp>
+#include <tmxpp/Unique_id.hpp>
+#include <tmxpp/Unit_interval.hpp>
+
+#endif // TMXPP_HPP
