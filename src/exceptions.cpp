16a39c17655826776f5c6ee318a258ec205d94ee
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-02-01 14:45:55 -0400

    Fix src/exceptions.cpp

--- src/exceptions.cpp
+++ src/exceptions.cpp
@@ -17,26 +17,26 @@ std::string quoted(std::string_view s)
 
 } // namespace
 
-explicit Invalid_attribute::Invalid_attribute(Name name)
-  : tmxpp::Invalid_attribute{"Invalid attribute " + quoted(get(name))};
+Invalid_attribute::Invalid_attribute(Name name)
+  : tmxpp::Invalid_attribute{"Invalid attribute " + quoted(get(name))}
 {
 }
 
-explicit Invalid_attribute::Invalid_attribute(Name name, Value value)
+Invalid_attribute::Invalid_attribute(Name name, Value value)
   : tmxpp::Invalid_attribute{"Invalid attribute value " + quoted(get(name)) +
-                             '=' + quoted(get(value))};
+                             '=' + quoted(get(value))}
 {
 }
 
-explicit Invalid_element::Invalid_element(Name name)
-  : tmxpp::Invalid_element{"Invalid element " + std::string{get(name)}};
+Invalid_element::Invalid_element(Name name)
+  : tmxpp::Invalid_element{"Invalid element " + std::string{get(name)}}
 {
 }
 
-explicit Invalid_element::Invalid_element(Name name, Value value)
+Invalid_element::Invalid_element(Name name, Value value)
   : tmxpp::Invalid_element{"Invalid element value <" + std::string{get(name)} +
-                           ">\n" + std::string{get(value)} +
-                           "\n<" std::string{get(name)} + '>'};
+                           ">\n" + std::string{get(value)} + "\n<" +
+                           std::string{get(name)} + '>'}
 {
 }
 
