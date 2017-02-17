7ed20e410a23c38372094f210a2cc62e7fb900f7
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-02-16 20:01:39 -0400

    Add to_point

---
+++ include/tmxpp/impl/to_point.hpp
@@ -0,0 +1,36 @@
+#ifndef TMXPP_IMPL_TO_POINT_HPP
+#define TMXPP_IMPL_TO_POINT_HPP
+
+#include <string>
+#include <string_view>
+#include <range/v3/begin_end.hpp>
+#include <range/v3/view/bounded.hpp>
+#include <range/v3/view/transform.hpp>
+#include <tmxpp/Point.hpp>
+#include <tmxpp/exceptions.hpp>
+#include <tmxpp/impl/read_utility.hpp>
+
+namespace tmxpp::impl {
+
+Point to_point(std::string_view point) try {
+    auto tokens{tokenize(point, ",")};
+    auto range{tokens |
+               ranges::view::transform([](std::string_view coordinate) {
+                   return from_string<Point::Coordinate>(coordinate);
+               }) |
+               ranges::view::bounded};
+
+    std::vector<Point::Coordinate> xy{ranges::begin(range), ranges::end(range)};
+
+    if (xy.size() != 2)
+        throw Exception{"Bad poly point: " + std::string{point}};
+
+    return {xy[0], xy[1]};
+}
+catch (const Exception& e) {
+    throw Exception{"Bad poly point: " + std::string{point} + ". " + e.what()};
+}
+
+} // namespace tmxpp::impl
+
+#endif // TMXPP_IMPL_TO_POINT_HPP
