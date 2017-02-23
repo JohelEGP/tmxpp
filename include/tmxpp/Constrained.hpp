22cfcad203b37400149fe6b0da2409da7239fc16
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-02-23 17:37:05 -0400

    Add Constrained component
    These aliases wrap an object of a given type, but restrict it to specified values. Invalid_argument is thrown for invalid values.

---
+++ include/tmxpp/Constrained.hpp
@@ -0,0 +1,44 @@
+#ifndef TMXPP_CONSTRAINED_HPP
+#define TMXPP_CONSTRAINED_HPP
+
+#include <jegp/Literal_constant.hpp>
+#include <jegp/utility.hpp>
+#include <type_safe/bounded_type.hpp>
+#include <type_safe/constrained_type.hpp>
+#include <tmxpp/exceptions.hpp>
+
+namespace tmxpp {
+
+struct Verifier {
+    template <class T, class Predicate>
+    static void verify(const T& value, Predicate pred)
+    {
+        if (!pred(value))
+            throw Invalid_argument{""};
+    }
+};
+
+template <class T, class Constraint>
+using Constrained = type_safe::constrained_type<T, Constraint, Verifier>;
+
+template <class T>
+using Non_empty = Constrained<T, type_safe::constraints::non_empty>;
+
+template <class T, class LowerConstant, class UpperConstant>
+using Closed_range = Constrained<
+    T, type_safe::constraints::closed_interval<
+           jegp::Value_type<LowerConstant>, LowerConstant, UpperConstant>>;
+
+template <class T, class ZeroConstant = jegp::Literal_constant<T>>
+using Positive = Constrained<
+    T, type_safe::constraints::greater<
+           jegp::Value_type<ZeroConstant>, ZeroConstant>>;
+
+template <class T, class ZeroConstant = jegp::Literal_constant<T>>
+using Non_negative = Constrained<
+    T, type_safe::constraints::greater_equal<
+           jegp::Value_type<ZeroConstant>, ZeroConstant>>;
+
+} // namespace tmxpp
+
+#endif // TMXPP_CONSTRAINED_HPP
--- include/tmxpp/Unit_interval.hpp
+++ include/tmxpp/Unit_interval.hpp
@@ -2,15 +2,13 @@
 #define TMXPP_UNIT_INTERVAL_HPP
 
 #include <boost/hana/integral_constant.hpp>
-#include <type_safe/bounded_type.hpp>
-#include <type_safe/constrained_type.hpp>
+#include <tmxpp/Constrained.hpp>
 
 namespace tmxpp {
 
 // A `double` that is in the [`0`,`1`] range.
-using Unit_interval = type_safe::constrained_type<
-    double, type_safe::constraints::closed_interval<
-                int, boost::hana::int_<0>, boost::hana::int_<1>>>;
+using Unit_interval =
+    Closed_range<double, boost::hana::int_<0>, boost::hana::int_<1>>;
 
 } // namespace tmxpp
 
