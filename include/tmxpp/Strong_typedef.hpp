503bd960bcda853c3e98335f56f64fcf283d4d42
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-03-19 15:23:41 -0400

    Update .clang-format

--- include/tmxpp/Strong_typedef.hpp
+++ include/tmxpp/Strong_typedef.hpp
@@ -6,11 +6,12 @@
 namespace tmxpp {
 
 template <class T, class Phantom>
-class Strong_typedef : public type_safe::strong_typedef<Phantom, T>,
-                       type_safe::strong_typedef_op::equality_comparison<
-                           Strong_typedef<T, Phantom>, bool>,
-                       type_safe::strong_typedef_op::relational_comparison<
-                           Strong_typedef<T, Phantom>, bool> {
+class Strong_typedef
+  : public type_safe::strong_typedef<Phantom, T>
+  , type_safe::strong_typedef_op::equality_comparison<
+        Strong_typedef<T, Phantom>, bool>
+  , type_safe::strong_typedef_op::relational_comparison<
+        Strong_typedef<T, Phantom>, bool> {
 public:
     using type_safe::strong_typedef<Phantom, T>::strong_typedef;
 };
