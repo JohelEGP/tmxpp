5e40607100f13dcfc83ea18d377606cc992750c6
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-03-19 17:24:58 -0400

    Update to type_safe library

--- include/tmxpp/Strong_typedef.hpp
+++ include/tmxpp/Strong_typedef.hpp
@@ -8,10 +8,10 @@ namespace tmxpp {
 template <class T, class Phantom>
 class Strong_typedef
   : public type_safe::strong_typedef<Phantom, T>
-  , type_safe::strong_typedef_op::equality_comparison<
-        Strong_typedef<T, Phantom>, bool>
-  , type_safe::strong_typedef_op::relational_comparison<
-        Strong_typedef<T, Phantom>, bool> {
+  , public type_safe::strong_typedef_op::equality_comparison<
+        Strong_typedef<T, Phantom>>
+  , public type_safe::strong_typedef_op::relational_comparison<
+        Strong_typedef<T, Phantom>> {
 public:
     using type_safe::strong_typedef<Phantom, T>::strong_typedef;
 };
