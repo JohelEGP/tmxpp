9494d9bba5f7d47716093ae4edcbd0675304ce87
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2017-03-26 13:10:32 -0400

    Fix CMake dependencies
    - Require Boost::boost unconditionally since it's used in the implementation. For simplicity, keep 1.61.0 as the required version.
    - Add missing type_safe target.

--- find_dependencies.cmake
+++ find_dependencies.cmake
@@ -1,6 +1,4 @@
-find_package(Boost.Hana QUIET)
-if(NOT Boost.Hana_FOUND)
-    find_package(Boost 1.61.0 REQUIRED)
-endif()
+find_package(Boost 1.61.0 REQUIRED)
+find_package(type_safe REQUIRED)
 find_package(jegp 3.1.0 REQUIRED)
 find_package(RapidXml 5.0.0 REQUIRED)
